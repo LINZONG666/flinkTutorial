4ebaf64f0630093e20639460fe4a13eefbd93a04
LINZONG888 <18717149380@126.com>
2019-08-20 20:13:27 +0800

    错误修改

--- src/main/scala/com/atguigu/wc/StreamWordCount.scala
+++ src/main/scala/com/atguigu/wc/StreamWordCount.scala
@@ -5,7 +5,7 @@ import org.apache.flink.streaming.api.scala._
 object StreamWordCount {
   def main(args: Array[String]): Unit = {
     val environment = StreamExecutionEnvironment.getExecutionEnvironment
-    environment.setParallelism()
+
     val testDS: DataStream[String] = environment.socketTextStream("hadoop102",7777)
     val streamWordCount: DataStream[(String, Int)] = testDS.flatMap(_.split("\\s"))
       .map((_, 1))
